9272a67d273b40b6ee53c5c4b717a8792607f28b
amp012 <102356853+amp012@users.noreply.github.com>
2022-03-26 10:01:11 +0530

    Add files via upload

---
+++ CameraAPI/MainActivity.java
@@ -0,0 +1,54 @@
+package com.mohsin.cameraapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgCamera;
+    Button btnCapture;
+    private static final int CAMERA_REQUEST = 1888;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imgCamera = findViewById(R.id.imgCamera);
+        btnCapture = findViewById(R.id.btnCapture);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST);
+                } else {
+                    Toast.makeText(MainActivity.this, "No permission granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imgCamera.setImageBitmap(image);
+        }
+    }
+}
